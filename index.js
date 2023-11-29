f0dcab4375f5092a7a6414692e57db9ae6f89f47
Haruto Nguyen <ngxvanquan@gmail.com>
2023-11-30 06:31:57 +0700

    fix some bugs

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const message = document.getElementById("message");
 messageLabel.addEventListener("click", () => {
     message.focus();
 });
+
+var onResize = (e) => {
+    e.style.height = "auto";
+    e.style.height = e.scrollHeight + 3 + "px";
+};
